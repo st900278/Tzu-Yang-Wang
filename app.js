38f494135ca31ba371c206f88d5d2c5d7c9aa8b8
oberonfrog <oberonfrog@gmail.com>
2022-10-23 23:02:35 +0900

    update

--- app.js
+++ app.js
@@ -2,21 +2,38 @@
 let Mustache = require('mustache');
 let fs = require('fs');
 
-let j = {}
+let indexJson = {}
+
+let projectJson = JSON.parse(fs.readFileSync('./static/json/project.json'));
+let projectTemp = fs.readFileSync('./static/mustache/project.mustache').toString();
+indexJson['project'] = Mustache.render(projectTemp, projectJson);
+
+
+
 let publicationJson = JSON.parse(fs.readFileSync('./static/json/publication.json'));
 let publicationTemp = fs.readFileSync('./static/mustache/publication.mustache').toString();
-j['publication'] = Mustache.render(publicationTemp, publicationJson);
+indexJson['publication'] = Mustache.render(publicationTemp, publicationJson);
 
 
 let grantJson = JSON.parse(fs.readFileSync('./static/json/grant.json'));
 let grantTemp = fs.readFileSync('./static/mustache/grant.mustache').toString();
-j['grant'] = Mustache.render(grantTemp, grantJson);
+indexJson['grant'] = Mustache.render(grantTemp, grantJson);
 
 let awardJson = JSON.parse(fs.readFileSync('./static/json/award.json'));
 let awardTemp = fs.readFileSync('./static/mustache/award.mustache').toString();
-j['award'] = Mustache.render(awardTemp, awardJson);
+indexJson['award'] = Mustache.render(awardTemp, awardJson);
 
 
 let indexTemp = fs.readFileSync('./static/mustache/index.mustache').toString();
 
-fs.writeFileSync("index.html", Mustache.render(indexTemp, j));
+fs.writeFileSync("index.html", Mustache.render(indexTemp, indexJson));
+
+let teamJson = {}
+let memberJson = JSON.parse(fs.readFileSync('./static/json/member.json'));
+let memberTemp = fs.readFileSync('./static/mustache/member.mustache').toString();
+teamJson['member'] = Mustache.render(memberTemp, memberJson); 
+
+let teamTemp = fs.readFileSync('./static/mustache/team.mustache').toString();
+
+
+fs.writeFileSync("team.html", Mustache.render(teamTemp, teamJson));
